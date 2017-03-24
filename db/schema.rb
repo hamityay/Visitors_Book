06fe1450e9b573459cc2e077288789713cbc3b85
hamityay <hamityay@hotmail.com>
2017-03-24 10:27:23 +0300

    Generated Interes model and fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309083407) do
+ActiveRecord::Schema.define(version: 20170324072554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 20170309083407) do
   add_index "cities", ["country_id"], name: "index_cities_on_country_id", using: :btree
 
   create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "interests", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
